4efd5278198d9d5d418a24c161ce9630b75aa829
teoma145 <matteodark145@yahoo.it>
2023-10-24 16:13:47 +0200

    ARRAYSTUDENTI

--- js/script.js
+++ js/script.js
@@ -11,3 +11,42 @@ const Segnaposto = tavolovip.map((elemento, index) => ({
 
 console.log(Segnaposto);
 
+const studenti=[
+    
+    {
+        id: 213 ,
+        stnome: 'Marco della Rovere',  
+        voto: 78,
+     },
+     {
+        id: 110 ,
+        stnome: 'Paola Cortellessa',  
+        voto: 96,
+     },
+     {
+        id: 250 ,
+        stnome: 'Andrea Mantegna ',  
+        voto: 48,
+     },
+     {
+        id: 145 ,
+        stnome: 'Gaia Borromini',  
+        voto: 74,
+     },
+     {
+        id: 196 ,
+        stnome: 'Luigi Grimaldello',  
+        voto: 68,
+     },
+     {
+        id: 102 ,
+        stnome: 'Piero della Francesca',  
+        voto: 78,
+     },
+     {
+        id: 120 ,
+        stnome: 'Francesca da Polenta',  
+        voto: 84,
+     },
+ 
+]
