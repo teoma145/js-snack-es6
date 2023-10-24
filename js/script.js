cedc2e04baafd2aa922a02222cc4a039991d57c6
teoma145 <matteodark145@yahoo.it>
2023-10-24 15:47:30 +0200

    array-object-with-map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const tavolovip=[
     'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'
 ]
+
+
+const Segnaposto = tavolovip.map((elemento, index) => ({
+    nome: elemento,
+    tavolo: 'tavolo ' + (index + 1),
+    nometavolo: 'Tavolo dei Vip'
+}));
+
+console.log(Segnaposto);
+
